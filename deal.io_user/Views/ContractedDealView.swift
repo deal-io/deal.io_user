3f615cad90b67160a810a515494162613a089647
tylerckeller <tylerkeller@mines.edu>
2023-02-28 19:51:03 -0700

    added the expanding functionality for card presses

---
+++ deal.io_user/Views/ContractedDealView.swift
@@ -0,0 +1,20 @@
+//
+//  ContractedDealView.swift
+//  deal.io_user
+//
+//  Created by Tyler Keller on 2/28/23.
+//
+
+import SwiftUI
+
+struct ContractedDealView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContractedDealView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContractedDealView()
+    }
+}
