7a2360c415c871c0f61a208afcb49c476a489800
tylerckeller <tylerkeller@mines.edu>
2023-02-28 19:32:33 -0700

    added switching between upcoming and daily feeds functionality

---
+++ deal.io_user/Views/DailyView.swift
@@ -0,0 +1,20 @@
+//
+//  DailyView.swift
+//  deal.io_user
+//
+//  Created by Tyler Keller on 2/28/23.
+//
+
+import SwiftUI
+
+struct DailyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DailyView_Previews: PreviewProvider {
+    static var previews: some View {
+        DailyView()
+    }
+}
