9d831e6072dccb9a978ba09a447421f549b914c1
Tyler Keller <tylerkeller@mines.edu>
2023-03-27 11:44:43 -0600

    refresh button for API call refreshing

--- deal.io_user/Views/FeedView.swift
+++ deal.io_user/Views/FeedView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct FeedView: View {
     
     @ObservedObject var viewModel: UserViewModel
+    @State var action: () -> Void = {}
+    @State var disabled = false
     let dailyDeals: [Deal]
     let upcomingDeals: [Deal]
     
@@ -20,42 +22,69 @@ struct FeedView: View {
     }
     
     var body: some View {
-        VStack {
-            Image("dealio_white_on_bg")
-                .resizable()
-                .frame(width: 200, height: 90)
-            if (viewModel.currentFeed == .UPCOMING) {
-                HStack {
-                    Spacer()
-                    DailyButton(fillColor: Deal_ioColor.background)
-                        .onTapGesture {
-                            viewModel.currentFeed = .DAILY
-                        }
-                    Spacer()
-                    UpcomingButton(fillColor: Deal_ioColor.selected)
-                        .onTapGesture {
-                            viewModel.currentFeed = .UPCOMING
-                        }
-                    Spacer()
-                }
-                UpcomingView(viewModel: viewModel, deals: self.upcomingDeals)
-            } else {
-                HStack {
-                    Spacer()
-                    DailyButton(fillColor: Deal_ioColor.selected)
-                        .onTapGesture {
-                            viewModel.currentFeed = .DAILY
-                        }
-                    Spacer()
-                    UpcomingButton(fillColor: Deal_ioColor.background)
-                        .onTapGesture {
-                            viewModel.currentFeed = .UPCOMING
-                        }
-                    Spacer()
-                }
-                DailyView(viewModel: viewModel, deals: self.dailyDeals)
+        if self.viewModel.deals.isEmpty {
+            VStack {
+                ProgressView("Loading deals...")
+                    .progressViewStyle(CircularProgressViewStyle(tint: Color.white))
+                    .scaleEffect(2.0)
+                    .padding()
             }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Deal_ioColor.background)
+        } else {
+            VStack {
+                ZStack {
+                    Image("dealio_white_on_bg")
+                        .resizable()
+                        .frame(width: 200, height: 90)
+                    Button(action: {
+                        action()
+                        viewModel.clear()
+                        viewModel.refresh()
+                        disabled = true
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                            disabled = false
+                        }
+                    }) {
+                        RefreshButton()
+                    }
+                    .disabled(disabled)
+                    .padding(.trailing, 290)
+                    
+                }
+                if (viewModel.currentFeed == .UPCOMING) {
+                    HStack {
+                        Spacer()
+                        DailyButton(fillColor: Deal_ioColor.background)
+                            .onTapGesture {
+                                viewModel.currentFeed = .DAILY
+                            }
+                        Spacer()
+                        UpcomingButton(fillColor: Deal_ioColor.selected)
+                            .onTapGesture {
+                                viewModel.currentFeed = .UPCOMING
+                            }
+                        Spacer()
+                    }
+                    UpcomingView(viewModel: viewModel, deals: self.upcomingDeals)
+                } else {
+                    HStack {
+                        Spacer()
+                        DailyButton(fillColor: Deal_ioColor.selected)
+                            .onTapGesture {
+                                viewModel.currentFeed = .DAILY
+                            }
+                        Spacer()
+                        UpcomingButton(fillColor: Deal_ioColor.background)
+                            .onTapGesture {
+                                viewModel.currentFeed = .UPCOMING
+                            }
+                        Spacer()
+                    }
+                    DailyView(viewModel: viewModel, deals: self.dailyDeals)
+                }
+            }
+            .background(Deal_ioColor.background)
         }
-        .background(Deal_ioColor.background)
     }
 }
